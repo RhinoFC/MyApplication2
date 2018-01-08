4931176438271d72a13957623ed9004d22884c2e
RhinoFC <rhinofc@yandex.ru>
2018-01-08 20:49:13 +0700

    This is my first Android project for Evotor devices

---
+++ app/src/main/java/com/example/rhinofc/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.rhinofc.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void onBtnClick(View v){
+        Toast.makeText(this, "hello", Toast.LENGTH_LONG).show();
+        textView.setText("Вот и хорошо!");
+
+    }
+
+}
